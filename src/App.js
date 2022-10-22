b56edd73a74880c1afe54a0b984be6237a2f85d0
Lara Bucher <bucher.lara@web.de>
2022-10-22 17:01:26 +0200

    add bookmark_page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import Card from "./Card/Card.js";
 import Navigation from "./Navbar/Navbar.js";
 import Header from "./Header/Header.js";
 import AddCard from "./Pages/AddCard.js";
@@ -7,60 +6,13 @@ import Home from "./Pages/Home.js";
 import Bookmark from "./Pages/Bookmark.js";
 import { useState } from "react";
 
-const tagItems = ["Bla", "blubb", "hallo", "Huhu", "dumm", "doof"];
-const tagItems2 = ["Mumps", "Masern", "Scharlach", "Röteln", "Pest", "Grippe"];
-
-const cardArray = [
-  {
-    id: 1,
-    question:
-      "In the Kingdom Heart series who provides the english voice for Master Eraqus?",
-    answer: "HUHU",
-    tags: tagItems,
-    isBookmarked: true,
-  },
-  {
-    id: 2,
-    question: "Lorem didorium?",
-    answer: "HAHAH",
-    tags: tagItems2,
-    isBookmarked: true,
-  },
-  {
-    id: 3,
-    question: "Warum stinkt Kevin?",
-    answer: "Weil er immer stinkt",
-    tags: ["Kacka", "Schweiß", "Pipi", "Gammel"],
-    isBookmarked: false,
-  },
-  {
-    id: 4,
-    question: "Warum ist Lea so dumm?",
-    answer: "Weil lea kein hirn hat",
-    tags: ["leer", "nixdrin", "dummdideldumm", "rülps"],
-    isBookmarked: true,
-  },
-];
-
 function App() {
   const [page, setPage] = useState("home");
-  console.log(page);
 
   return (
     <div className="App">
       <Header />
       <main>
-        <ul className="card-list">
-          {cardArray.map((card) => (
-            <Card
-              key={card.id}
-              question={card.question}
-              answer={card.answer}
-              tags={card.tags}
-              isBookmarked={card.isBookmarked}
-            />
-          ))}
-        </ul>
         {page === "home" && <Home />}
         {page === "bookmark" && <Bookmark />}
         {page === "profile" && <Profile />}
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,3 +1,51 @@
+import Card from "../Card/Card.js";
+
+const tagItems = ["Bla", "blubb", "hallo", "Huhu", "dumm", "doof"];
+const tagItems2 = ["Mumps", "Masern", "Scharlach", "Röteln", "Pest", "Grippe"];
+const cardArray = [
+  {
+    id: 1,
+    question:
+      "In the Kingdom Heart series who provides the english voice for Master Eraqus?",
+    answer: "HUHU",
+    tags: tagItems,
+    isBookmarked: true,
+  },
+  {
+    id: 2,
+    question: "Lorem didorium?",
+    answer: "HAHAH",
+    tags: tagItems2,
+    isBookmarked: true,
+  },
+  {
+    id: 3,
+    question: "Warum stinkt Kevin?",
+    answer: "Weil er immer stinkt",
+    tags: ["Kacka", "Schweiß", "Pipi", "Gammel"],
+    isBookmarked: false,
+  },
+  {
+    id: 4,
+    question: "Warum ist Lea so dumm?",
+    answer: "Weil lea kein hirn hat",
+    tags: ["leer", "nixdrin", "dummdideldumm", "rülps"],
+    isBookmarked: true,
+  },
+];
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <ul className="card-list">
+      {cardArray.map((card) => (
+        <Card
+          key={card.id}
+          question={card.question}
+          answer={card.answer}
+          tags={card.tags}
+          isBookmarked={card.isBookmarked}
+        />
+      ))}
+    </ul>
+  );
 }
